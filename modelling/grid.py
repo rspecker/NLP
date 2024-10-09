85dc9790fabf86cffe7fe5049723950676a3e392
Richard Specker <91221074+rspecker@users.noreply.github.com>
2024-10-09 13:37:25 +0200

    :sparkles: created function for modelling

---
+++ modelling/grid.py
@@ -0,0 +1,25 @@
+
+# Define hyperparameters for tuning
+svc_param_grid = {
+    'C': [0.1, 1, 10, 100],
+    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+    'degree': [2, 3, 4],  # Used with 'poly' kernel
+    'gamma': ['scale', 'auto'],
+    'class_weight': [None, 'balanced']
+}
+
+# Define hyperparameters for tuning
+rf_param_grid = {
+    'n_estimators': [100, 200, 300, 400],
+    'max_depth': [None, 10, 20, 30, 40],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False],
+    'max_features': ['auto', 'sqrt', 'log2']
+}
+
+# Define hyperparameters for tuning
+mnb_param_grid = {
+    'alpha': [0.1, 0.5, 1.0, 2.0, 5.0],
+    'fit_prior': [True, False]
+}
