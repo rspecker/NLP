42fda2e9e9664781d682b7bd6a607c84d2715554
Richard Specker <91221074+rspecker@users.noreply.github.com>
2024-10-09 14:29:15 +0200

    Added function for train test split

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from sklearn.model_selection import train_test_split
 
 from preprocessing.preproc import preproc
 from preprocessing.tfidf import create_train_df_tfidf
+from utils import create_train_test_sets
 
 if __name__ == "__main__":
     # Import data
     df = pd.read_table('train.txt',
                        names=['title', 'from', 'genre', 'director', 'plot'])
     # Split data into training and testing sets
-    train_df, test_df = train_test_split(df, test_size=0.20, random_state=0)
+    x_train, x_test, y_train, y_test = create_train_test_sets(
+        df, test_size=0.2, random_state=0, y_column='genre'
+    )
+
 
     print("Before preproc: ", df['plot'][0])
     print("After preproc: ", preproc(df['plot'][0]))
