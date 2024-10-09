2811cf71937f16813809121c391341bcda1dfc73
Richard Specker <91221074+rspecker@users.noreply.github.com>
2024-10-09 14:18:41 +0200

    added train test split

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     # Import data
     df = pd.read_table('train.txt',
                        names=['title', 'from', 'genre', 'director', 'plot'])
-
+    # Split data into training and testing sets
+    train_df, test_df = train_test_split(df, test_size=0.20, random_state=0)
 
     print("Before preproc: ", df['plot'][0])
     print("After preproc: ", preproc(df['plot'][0]))
