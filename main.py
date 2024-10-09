fa852dca50d0fc4ddeb3a9c35ae25612331dd52e
Richard Specker <91221074+rspecker@users.noreply.github.com>
2024-10-09 14:17:58 +0200

    reformated file

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from preprocessing.preproc import preproc
 from preprocessing.tfidf import create_train_df_tfidf
 
-
 if __name__ == "__main__":
-  df = pd.read_table('train.txt', names=['title', 'from', 'genre', 'director', 'plot'])
-  #print(df)
-
-  print("Before preproc: ", df['plot'][0])
-  print("After preproc: ", preproc(df['plot'][0]))
-
- 
-  create_train_df_tfidf(df)
- 
-  
+    # Import data
+    df = pd.read_table('train.txt',
+                       names=['title', 'from', 'genre', 'director', 'plot'])
 
 
+    print("Before preproc: ", df['plot'][0])
+    print("After preproc: ", preproc(df['plot'][0]))
+
+    create_train_df_tfidf(df)
