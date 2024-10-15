36b8ab339b11a71795de9fe7f94eecfac6b0b196
Richard Specker <91221074+rspecker@users.noreply.github.com>
2024-10-15 12:40:44 +0200

    Update grid.py

--- modelling/grid.py
+++ modelling/grid.py
@@ -1,10 +1,13 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
 # Define hyperparameters for tuning for SVC
 svc_param_grid = {
     'C': [0.1, 1, 10],
-    'kernel': ['linear'], # , 'rbf'],
-    # 'gamma': ['scale', 'auto'],
-    # 'class_weight': [None, 'balanced']
+    'kernel': ['linear', 'rbf'],
+    'gamma': ['scale', 'auto'],
+    'class_weight': [None, 'balanced']
 }
 
 # Define hyperparameters for tuning for RandomForestClassifier
@@ -20,5 +23,11 @@ rf_param_grid = {
 # Define hyperparameters for tuning for MultinomialNB
 mnb_param_grid = {
     'alpha': [0.1, 0.5, 1.0, 2.0, 5.0],
-    'fit_prior': [True, False]
+}
+
+# Define the models
+models = {
+    'SVC': (SVC(), svc_param_grid),
+    'RandomForest': (RandomForestClassifier(), rf_param_grid),
+    'MultinomialNB': (MultinomialNB(), mnb_param_grid)
 }
